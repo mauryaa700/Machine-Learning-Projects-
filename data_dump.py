1ff9d8d23bde637b93a00983a25eee0b58e44a13
mauryaa700 <mr.mauryaa@gmail.com>
2022-12-26 02:32:05 +0530

    remove one blank space from data_dump.py file

--- data_dump.py
+++ data_dump.py
@@ -4,7 +4,6 @@ import json
 # Provide the mongodb localhost url to connect python to mongodb.
 client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
 DATA_FILE_PATH="/config/workspace/aps_failure_training_set1.csv"
-
 DATABASE_NAME="aps"
 COLLECTION_NAME="sensor"
 
